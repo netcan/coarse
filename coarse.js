2f4aca3e5cadb408ebe232f86082fcec6ff71d47
netcan <1469709759@qq.com>
2021-02-02 23:23:31 +0800

    update param

--- coarse.js
+++ coarse.js
@@ -10,7 +10,12 @@ const inputFile = process.argv[2]
 const outputFile = process.argv[3]
 
 const original = fs.readFileSync(inputFile)
-const roughened = coarse(original, {strokeWidth: 2})
+const roughened = coarse(original, {
+    strokeWidth: 1,
+    roughness: 0.6,
+    bowing: 0.3,
+    fillStyle:'hachure'} // hachure(default), solid, zigzag, cross-hatch, dots, dashed, or zigzag-line
+)
 const parsed = parser.parse(roughened)
 const converted = convertDocumentForeignObjectsToText(parsed)
 const serialized = xmlserializer.serializeToString(converted)
